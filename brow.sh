10234d67ec1d64eed9334de9a2d1a6c1d1ecf8e9
Shaun <shaunferns99@gmail.com>
2019-04-27 22:43:31 +0400

    Add while loop for entry, and 'b' and 'q options

--- brow.sh
+++ brow.sh
@@ -1,11 +1,29 @@
-printf "Enter a website: "
-read site
-terminal=/dev/pts/1
-columns=$(stty -a <"$terminal" | grep -Po '(?<=columns )\d+')
-cols=$(tput cols)
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
 
-printf "Displaying website below: \n"
-s=$(printf "%-${cols}s" "_")
-echo "${s// /_}"
-echo ""
-links -dump $site
+clear
+while true; do
+	printf "Options: \n"
+	printf "────────\n"
+	printf "enter 'b' to go back\n"
+	printf "enter 'q' to quit\n"
+	printf "Enter a URL to display content\n\n"
+	read -p "Input: " choice
+	
+	if [ $choice == 'b' ]; then
+		printf "Going to previous website...\n"
+	elif [ $choice == 'q' ]; then
+		printf "Quitting ...\n"
+		break
+	else
+		cols=$(( $(tput cols) + 2 ))
+		s=$(printf "%-${cols}s" "─")
+		
+		clear
+		printf "website: %s \n" "$choice"
+		
+		echo -e "${CYAN}${s// /─}${NC}"
+		links -dump $choice
+		echo -e "${CYAN}${s// /─}${NC}\n"
+	fi
+done
